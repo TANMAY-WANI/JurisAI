b6901b319ed95f9f39a89df929dfa844e0603f64
TANMAY-WANI <tanmaywani145@gmail.com>
2024-07-31 20:33:00 +0530

    Added the Summary generation using GenAI

--- JurisAI/Service/views.py
+++ JurisAI/Service/views.py
@@ -4,6 +4,18 @@ from Service.forms import DocumentForm
 import PyPDF2
 # Create your views here.
 
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API"))
+model = genai.GenerativeModel('gemini-pro')
+
+def get_summary_from_llm(text_input):
+    res= model.generate_content(text_input+"\n Summarize the given text in not more than 500 words. give only the summary and not anything else, like the introduction line or concluding line.")
+    return res
+
 def chat(request):
     return render(request,'chat.html')
 
@@ -18,6 +30,9 @@ def get_summary(request):
             for page_num in range(len(pdf_reader.pages)):
                 page = pdf_reader.pages[page_num]
                 text += page.extract_text()
+
+            response = get_summary_from_llm(text)
+
+
         
-        
-    return JsonResponse({'summary':"Hello world, this is tanmay wani. You are currenly viewing my summary as a response"})
+    return JsonResponse({'summary':response.text})
